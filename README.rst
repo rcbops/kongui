55d81d441a63c254d607c60862d1cb068eb58850
rpedde <ron@pedde.com>
2011-11-30 10:38:21 -0600

    Initial checkin

---
+++ README.rst
@@ -0,0 +1,30 @@
+::
+
+:::    ::: ::::::::  ::::    :::  ::::::::  :::    ::: :::::::::::
+:+:   :+: :+:    :+: :+:+:   :+: :+:    :+: :+:    :+:     :+:
++:+  +:+  +:+    +:+ :+:+:+  +:+ +:+        +:+    +:+     +:+
++#++:++   +#+    +:+ +#+ +:+ +#+ :#:        +#+    +:+     +#+
++#+  +#+  +#+    +#+ +#+  +#+#+# +#+   +#+# +#+    +#+     +#+
+#+#   #+# #+#    #+# #+#   #+#+# #+#    #+# #+#    #+#     #+#
+###    ### ########  ###    ####  ########   ########  ###########
+
+kongui
+======
+
+simple splinter-based tests against the dashboard
+
+
+Quickstart
+----------
+
+You're going to want to make your own config.ini file in the /etc/ directory,
+it needs to point at your running dashboard.
+
+After that, do::
+
+  run_tests.sh
+
+Additional Info
+---------------
+
+There are additional README files in the various subdirectories of this project.
---
+++ tests/__init__.py
@@ -0,0 +1,108 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010-2011 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import ConfigParser
+import nose.plugins.skip
+import os
+from pprint import pprint
+import unittest2
+# from xmlrpclib import Server
+
+
+class skip_test(object):
+    """Decorator that skips a test."""
+    def __init__(self, msg):
+        self.message = msg
+
+    def __call__(self, func):
+        def _skipper(*args, **kw):
+            """Wrapped skipper function."""
+            raise nose.SkipTest(self.message)
+        _skipper.__name__ = func.__name__
+        _skipper.__doc__ = func.__doc__
+        return _skipper
+
+
+class skip_if(object):
+    """Decorator that skips a test."""
+    def __init__(self, condition, msg):
+        self.condition = condition
+        self.message = msg
+
+    def __call__(self, func):
+        def _skipper(*args, **kw):
+            """Wrapped skipper function."""
+            if self.condition:
+                raise nose.SkipTest(self.message)
+            func(*args, **kw)
+        _skipper.__name__ = func.__name__
+        _skipper.__doc__ = func.__doc__
+        return _skipper
+
+
+class skip_unless(object):
+    """Decorator that skips a test."""
+    def __init__(self, condition, msg):
+        self.condition = condition
+        self.message = msg
+
+    def __call__(self, func):
+        def _skipper(*args, **kw):
+            """Wrapped skipper function."""
+            if not self.condition:
+                raise nose.SkipTest(self.message)
+            func(*args, **kw)
+        _skipper.__name__ = func.__name__
+        _skipper.__doc__ = func.__doc__
+        return _skipper
+
+
+class FunctionalTest(unittest2.TestCase):
+    @classmethod
+    def setUpClass(self):
+        # Setup project hashes
+        self.dash = {}
+
+        #TODO: need to move this stuff out to util files
+        def parse_config_file(self):
+            cfg = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   "..", "etc", "config.ini"))
+            if os.path.exists(cfg):
+                ret_hash = _build_config(self, cfg)
+            else:
+                raise Exception("Cannot read %s" % cfg)
+            return ret_hash
+
+        def _build_config(self, config_file):
+            parser = ConfigParser.ConfigParser()
+            parser.read(config_file)
+            ret_hash = {}
+            for section in parser.sections():
+                ret_hash[section] = {}
+                for value in parser.options(section):
+                    ret_hash[section][value] = parser.get(section, value)
+            return ret_hash
+
+        self.config = parse_config_file(self)
+
+    @classmethod
+    def tearDownClass(self):
+        self.config = ""
+
+    def setUp(self):
+        pass
+
